44c7e9b3f7fa8aee63024ce2e1d2764801251428
Michael Fahim <michael_fahim@yahoo.com>
2020-07-19 16:12:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,11 +8,23 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    erb :reversed
-  end
+  original_string = params["string"]
+  @reversed_string = original_string.reverse
+
+  erb :reversed
+end
 
   get '/friends' do
     # Write your code here!
 
   end
+  get "/" do
+  @user = "Ian"
+
+  erb :index # @user will be defined as 'Ian' in the view
+end
+
+get "/profile" do
+  erb :profile # @user will be nil here
+end
 end
